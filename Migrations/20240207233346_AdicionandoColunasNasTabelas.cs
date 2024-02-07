cb59f083d1462917e45ac1a235e781b90882c777
WellingtonBCMoreira <wellington.moreira@fieb.org.br>
2024-02-07 20:35:31 -0300

    executando o ef update

---
+++ Migrations/20240207233346_AdicionandoColunasNasTabelas.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CamposRepresentacoes.Migrations
+{
+    public partial class AdicionandoColunasNasTabelas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Codigo",
+                table: "Produtos",
+                type: "TEXT",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FormaPagamento",
+                table: "Pedidos",
+                type: "TEXT",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Preco",
+                table: "ItensPedido",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Codigo",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "FormaPagamento",
+                table: "Pedidos");
+
+            migrationBuilder.DropColumn(
+                name: "Preco",
+                table: "ItensPedido");
+        }
+    }
+}
